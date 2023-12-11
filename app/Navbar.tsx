ddf6b82fff7dc664358141f8c4262d990b9698f6
Nazifullah <nazif.it@gmail.com>
2023-12-11 14:05:04 +0530

    Navbar_ styling active link

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,12 +1,16 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import logo from "../public/Logos/logo-png.png";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+import classname from "classnames";
 
 const Navbar = () => {
+  const current = usePathname();
   const links = [
     { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/" },
+    { label: "Issues", href: "/issues" },
   ];
   return (
     <nav className="flex mb-5 space-x-6 items-center h-14 border-b px-5">
@@ -19,7 +23,15 @@ const Navbar = () => {
           <Link
             key={link.href}
             href={link.href}
-            className="space-x-6 text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classname({
+              "text-zinc-800": link.href === current,
+              "text-zinc-500": link.href !== current,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+
+            // {` ${
+            //   link.href === current ? "text-zinc-800" : " text-zinc-500"
+            // } space-x-6 `}
           >
             {link.label}{" "}
           </Link>
