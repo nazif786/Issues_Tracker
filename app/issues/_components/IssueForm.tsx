34db44b4287037008e374423c39cc8d47399fd96
Nazifullah <nazif.it@gmail.com>
2024-01-16 05:22:32 +0530

    build the issue edit page

---
+++ app/issues/_components/IssueForm.tsx
@@ -0,0 +1,98 @@
+"use client";
+import { Button, Callout, Heading, TextField, Text } from "@radix-ui/themes";
+import dynamic from "next/dynamic";
+import "easymde/dist/easymde.min.css";
+import { Controller, useForm } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { issueSchema } from "@/app/validationSchemas";
+import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
+import { Issue } from "@prisma/client";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
+type IssueFormData = z.infer<typeof issueSchema>;
+
+const NewIssuePage = ({ issue }: { issue?: Issue }) => {
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueFormData>({
+    resolver: zodResolver(issueSchema),
+  });
+  const router = useRouter();
+  const [error, setError] = useState("");
+  const [isSubmitting, setSubmitting] = useState(false);
+  return (
+    <div className="px-5 max-w-full">
+      {error && (
+        <Callout.Root>
+          <Callout.Text color="red">{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3 "
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            setSubmitting(true);
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setSubmitting(false);
+            setError("Unexpencted Error Happened");
+          }
+        })}
+      >
+        <Heading>Add New Issue </Heading>
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
+        <TextField.Root>
+          <TextField.Input
+            defaultValue={issue?.title}
+            placeholder="Title"
+            {...register("title")}
+          />
+        </TextField.Root>
+        <ErrorMessage>{errors.discription?.message}</ErrorMessage>
+        <Controller
+          defaultValue={issue?.discription}
+          name="discription"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Discription... " {...field} />
+          )}
+        />
+        {/* <TextArea  /> */}
+        <Button disabled={isSubmitting}>
+          Submit New Issue {isSubmitting && <Spinner />}
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default NewIssuePage;
+
+//-------- if there is issue with navigator not defined then use this code instead
+
+// onSubmit={handleSubmit(async (data) => {
+//   try {
+//       const response = await fetch("/api/issues", {
+//           method: "POST",
+//           headers: {
+//               "Content-Type": "application/json",
+//           },
+//           body: JSON.stringify(data),
+//       });
+//       if (!response.ok) throw new Error();
+//       router.push("/issues");
+//   } catch (error) {
+//       setError("An unexpected error occurred.");
+//   }
+// })}
