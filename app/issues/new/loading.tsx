1b2b3a22f0673079e5a311046d506ef385e780d2
Nazifullah <nazif.it@gmail.com>
2024-01-15 07:13:12 +0530

    build new issue page skeleton

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,18 @@
+import { Box, Heading } from "@radix-ui/themes";
 import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const NewIssueLoading = () => {
-  return <div>Loading...</div>;
+  return (
+    <Box className="px-5 max-w-full">
+      <Heading>Add New Issue </Heading>
+      <Box className="max-w-xl">
+        <Skeleton height="2rem" />
+        <Skeleton height="20rem" />
+      </Box>
+    </Box>
+  );
 };
 
 export default NewIssueLoading;
