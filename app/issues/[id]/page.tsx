2f981c02fb63b103256e5d7fabe43315c9ab1a71
Nazifullah <nazif.it@gmail.com>
2024-01-15 13:51:45 +0530

    refactor single resposibility principle

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,11 +1,9 @@
-import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import prisma from "@/prisma/client";
-import { Pencil2Icon } from "@radix-ui/react-icons";
-import { Box, Button, Card, Flex, Grid, Heading } from "@radix-ui/themes";
+import { Box, Grid } from "@radix-ui/themes";
 import delay from "delay";
-import Link from "next/link";
 import { notFound } from "next/navigation";
-import Markdown from "react-markdown";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
 
 const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
   //   if (typeof params.id !== "number") notFound();
@@ -18,20 +16,10 @@ const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
   return (
     <Grid columns={{ initial: "1", md: "2" }} gap="5">
       <Box className="mt-10 mx-3">
-        <Heading>{issue.title}</Heading>
-        <Flex gap="3" my="3">
-          <IssueStatusBadge status={issue.status} />
-          <p>{issue.createdAt.toDateString()}</p>
-        </Flex>
-        <Card className="prose" mt="5">
-          <Markdown>{issue.discription}</Markdown>
-        </Card>
+        <IssueDetails issue={issue} />
       </Box>
       <Box>
-        <Button>
-          <Pencil2Icon />
-          <Link href={`/issues/${issue.id}/edit`}>Edit Issue </Link>
-        </Button>
+        <EditIssueButton issueId={issue.id} />
       </Box>
     </Grid>
   );
