881ed02772e3aad6133d110caf1f843b09209457
Nazifullah <nazif.it@gmail.com>
2024-01-16 05:49:29 +0530

    build api to update issues

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,37 @@
+import { issueSchema } from "@/app/validationSchemas";
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
+
+export async function Patch(
+  request: NextRequest,
+  {
+    params,
+  }: {
+    params: {
+      id: string;
+    };
+  }
+) {
+  const body = await request.json();
+  const validData = issueSchema.safeParse(body);
+
+  if (!validData.success)
+    return NextResponse.json(validData.error.format(), { status: 201 });
+
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue)
+    return NextResponse.json({ error: "Invalid Issue" }, { status: 404 });
+
+  const updatedIssue = await prisma.issue.update({
+    where: { id: issue.id },
+    data: {
+      title: body.title,
+      discription: body.discription,
+    },
+  });
+
+  return NextResponse.json(updatedIssue);
+}
