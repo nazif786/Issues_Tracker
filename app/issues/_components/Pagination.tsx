e3207403d10955a78cb9a874fa3b09b8e59462ae
Nazifullah <nazif.it@gmail.com>
2024-01-30 13:01:52 +0530

    implement pagination

--- app/issues/_components/Pagination.tsx
+++ app/issues/_components/Pagination.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   ArrowLeftIcon,
   ArrowRightIcon,
@@ -5,31 +6,61 @@ import {
   DoubleArrowRightIcon,
 } from "@radix-ui/react-icons";
 import { Button, Flex, Text } from "@radix-ui/themes";
-import React from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+
 interface Props {
   itemCount: number;
   pageSize: number;
   currentPage: number;
 }
 const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
   const pageCout = Math.ceil(itemCount / pageSize);
   if (pageCout <= 1) return null;
+
+  const changePage = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    router.push("?" + params.toString());
+  };
+
   return (
     <Flex align="center" gap="3">
       <Text size="2">
         Page {currentPage} of {pageCout}
       </Text>
-      <Button variant="soft" color="gray" disabled={currentPage === 1}>
+      <Button
+        variant="soft"
+        color="gray"
+        disabled={currentPage === 1}
+        onClick={() => changePage(1)}
+      >
         <DoubleArrowLeftIcon />
       </Button>
-      <Button variant="soft" color="gray" disabled={currentPage === 1}>
+      <Button
+        variant="soft"
+        color="gray"
+        disabled={currentPage === 1}
+        onClick={() => changePage(currentPage - 1)}
+      >
         <ArrowLeftIcon />
       </Button>
-      <Button variant="soft" color="gray" disabled={currentPage === pageCout}>
+      <Button
+        variant="soft"
+        color="gray"
+        disabled={currentPage === pageCout}
+        onClick={() => changePage(currentPage + 1)}
+      >
         <ArrowRightIcon />
       </Button>
 
-      <Button variant="soft" color="gray" disabled={currentPage === pageCout}>
+      <Button
+        variant="soft"
+        color="gray"
+        disabled={currentPage === pageCout}
+        onClick={() => changePage(pageCout)}
+      >
         <DoubleArrowRightIcon />
       </Button>
     </Flex>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 import Image from "next/image";
 import Pagination from "./issues/_components/Pagination";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
   return (
     <div>
       <p> hello issue tracker </p>
-      <Pagination itemCount={100} pageSize={10} currentPage={3} />
+      <Pagination
+        itemCount={100}
+        pageSize={10}
+        currentPage={parseInt(searchParams.page)}
+      />
     </div>
   );
 }
